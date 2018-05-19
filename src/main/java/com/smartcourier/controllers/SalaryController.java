0f5e9d45b12540141f306feae4555fe27a080f7c
Barak Segal <barak353@gmail.com>
2018-05-19 17:16:49 +0300

    Can now create Salary and get all user with their agents and salaries.

--- src/main/java/com/smartcourier/controllers/SalaryController.java
+++ src/main/java/com/smartcourier/controllers/SalaryController.java
@@ -5,12 +5,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.smartcourier.beans.Agent;
 import com.smartcourier.beans.Salary;
 import com.smartcourier.dao.AgentDao;
 import com.smartcourier.dao.SalaryDao;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(path = "/salary")
@@ -28,6 +33,20 @@ public class SalaryController {
 	@GetMapping("/getAll")
 	public List<Salary> getAllSalary(){
 		return salaryDao.findAll();
+	}
+	
+	@ApiOperation(value="Create salary", response= Iterable.class)
+	@PostMapping("/create/{agentId}")
+	public Salary createSalary(@PathVariable(value = "agentId") Long agentId, @RequestBody Salary salary) {
+		Agent agent = agentDao.findOne(agentId);
+		if(agent != null){
+			agentDao.delete(agent);
+			agent.getSalary().add(salary);
+			agentDao.save(agent);
+			return salary;
+		} else{
+			return null;
+		}
 	}
 	
 }
