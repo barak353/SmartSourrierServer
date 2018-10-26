4cee87203942cfaa3696a82b6f5c461f5f631edd
Barak Segal <barak353@gmail.com>
2018-10-26 22:01:03 +0300

    Fix warnings in eclipse.

--- src/main/java/com/smartcourier/dao/CourierDao.java
+++ src/main/java/com/smartcourier/dao/CourierDao.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.smartcourier.beans.Courier;
-import com.smartcourier.beans.User;
 
 @Repository
 public interface CourierDao extends JpaRepository<Courier, Long>{
